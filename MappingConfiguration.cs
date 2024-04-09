c6e67dd1da217f38d4364a1a219a10601200f44a
Mark Christopher R. Cacal <markchristopher.cacal@gmail.com>
2024-04-09 23:06:27 +0800

    Add project files.

---
+++ MappingConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace DotMapper;
+
+public class MappingConfiguration
+{
+    public string SourceName { get; set; }
+    public string DestinationName { get; set; }
+    public List<MappingDictionary> MappingDictionary { get; set; } = new();
+}
+
+public class MappingDictionary
+{
+    public Expression SourceExpression { get; set; }
+    public Expression DestinationExpression { get; set; }
+    public object SourceFunc { get; set; }
+    public object DestinationFunc { get; set; }
+}
